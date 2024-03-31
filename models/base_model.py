22836f73888b9d45d21535fb93976ddbf485791b
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-03-31 05:41:55 -0700

    third

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,23 @@ class BaseModel:
     atrributes/methods from
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize base model attributes
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(
+                                value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
